9e7c448002cb509d369819ed44f0e40272daf7ce
mwoodbri <mark.woodbridge@cantab.net>
2016-01-01 23:22:09 +0000

    0.5.0-2.10.6

--- package.js
+++ package.js
@@ -1,5 +1,5 @@
 Package.describe({
-  version: '0.4.1-2.10.6',
+  version: '0.5.0-2.10.6',
   name: 'chaser:moment-timezone',
   summary: 'Moment Timezone for Meteor',
   documentation: null
@@ -7,10 +7,10 @@ Package.describe({
 
 Package.onUse(function(api) {
   api.addFiles([
-    'node_modules/moment-timezone/node_modules/moment/moment.js',
-    'node_modules/moment-timezone/node_modules/moment/locale/en-gb.js',
-    'node_modules/moment-timezone/node_modules/moment/locale/en-au.js',
-    'node_modules/moment-timezone/node_modules/moment/locale/en-ca.js',
+    'node_modules/moment/moment.js',
+    'node_modules/moment/locale/en-gb.js',
+    'node_modules/moment/locale/en-au.js',
+    'node_modules/moment/locale/en-ca.js',
     'node_modules/moment-timezone/moment-timezone.js',
     'chaser:moment-timezone.js',
     'tzdata.js'
