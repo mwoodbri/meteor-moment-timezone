1080d72756b9f56dab7e9ad20431d1267dc165e0
mwoodbri <mark.woodbridge@chaser.io>
2016-03-02 09:16:47 +0000

    0.5.1-2016a-2.11.2

--- package.js
+++ package.js
@@ -1,5 +1,5 @@
 Package.describe({
-  version: '0.5.0-2015g-2.11.2',
+  version: '0.5.1-2016a-2.11.2',
   name: 'chaser:moment-timezone',
   summary: 'Moment Timezone for Meteor',
   documentation: null
